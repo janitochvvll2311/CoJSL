062e7fe954558dd6f5e6adf9818026d7d6349190
CHAVA <janitochvvll2311@gmail.com>
2023-02-14 15:52:40 -0600

    Cleanup

--- index.js
+++
@@ -1,7 +0,0 @@
-import { useLayouts, useTemplateLayouts } from "./lib/layout.js";
-import { useViews } from "./lib/view.js";
-
-await useTemplateLayouts();
-await useLayouts();
-let views = useViews();
-console.log(views);
