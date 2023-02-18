d152988ad4d04aae87d43b4970051b4f66d73b32
CHAVA <janitochvvll2311@gmail.com>
2023-02-17 19:30:41 -0600

    Plain DOM component

--- exmples/playground/index.js
+++ exmples/playground/index.js
@@ -1,11 +1,5 @@
 import { Component } from "../../lib/component.js";
 
-class CustomComponent extends Component {
-
-    constructor() {
-        super("./custom.html");
-    }
-
-}
-
-customElements.define("co-custom", CustomComponent);
+customElements.define("co-custom", class extends Component {
+    constructor() { super("./custom.html"); }
+});
