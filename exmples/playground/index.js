c336139d826399a2ec890d1e74960c83ca6c7968
CHAVA <janitochvvll2311@gmail.com>
2023-02-17 19:32:34 -0600

    Component on create callback added

--- exmples/playground/index.js
+++ exmples/playground/index.js
@@ -2,4 +2,9 @@ import { Component } from "../../lib/component.js";
 
 customElements.define("co-custom", class extends Component {
     constructor() { super("./custom.html"); }
+
+    onCreate() {
+        console.log("It Works");
+    }
+
 });
